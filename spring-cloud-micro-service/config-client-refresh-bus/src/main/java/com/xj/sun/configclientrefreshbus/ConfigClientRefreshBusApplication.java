1bb4ec5f124df45382809127a023d2e59bc34ef4
SunXiaojun1008 <1648171941@qq.com>
2018-07-27 17:27:46 +0800

    T
    T

--- spring-cloud-micro-service/config-client-refresh-bus/src/main/java/com/xj/sun/configclientrefreshbus/ConfigClientRefreshBusApplication.java
+++ spring-cloud-micro-service/config-client-refresh-bus/src/main/java/com/xj/sun/configclientrefreshbus/ConfigClientRefreshBusApplication.java
@@ -2,11 +2,13 @@ package com.xj.sun.configclientrefreshbus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigClientRefreshBusApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigClientRefreshBusApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientRefreshBusApplication.class, args);
+	}
 }
---
+++ spring-cloud-micro-service/config-client-refresh-bus/src/main/java/com/xj/sun/configclientrefreshbus/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.xj.sun.configclientrefreshbus.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Copyright (C), 2017-2018, sunxiaojun
+ * FileName: com.xj.sun.provideruser.controller
+ * Author:   孙
+ * Date:    2018/7/26 15:08
+ * Description: //模块目的、功能描述
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${info.app.java.source}")
+    private String ipAddress;
+
+    @GetMapping("/profile")
+    public String hello(){
+        return this.ipAddress;
+    }
+}
